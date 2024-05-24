e7e6e345222fd58e79831912543f93b25402ebe9
Dennis <dwcreagh@users.noreply.github.com>
2024-05-24 12:36:26 -0700

    updates to priority

--- Bulk Merge/AppendDict.py
+++ Bulk Merge/AppendDict.py
@@ -1,8 +1,7 @@
-#for each key in the master dictionary this function will append the values from the the lead dictionary
-#for that same key
-def appendDict (dict_master, lead_dict):
-
+# for each key in the master dictionary this function will append the values
+# from the lead dictionary for that same key
+def appendDict(dict_master, lead_dict):
     for line in lead_dict:
-        dict_master[line] = dict_master[line]+[lead_dict[line]]
+        dict_master[line] = dict_master[line] + [lead_dict[line]]
 
     return dict_master
